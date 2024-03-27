5275215e7d68d40fdd25dbacd43f2d6d6e6f190c
ToThienHung-NLU <20130274@st.hcmuaf.edu.vn>
2024-03-27 21:40:54 +0700

    update validation

--- src/schema/formSchema.js
+++ src/schema/formSchema.js
@@ -9,8 +9,11 @@ export const registerSchema = yup.object().shape({
 
   password: yup
     .string()
+    .required("Vui lòng nhập mật khẩu")
     .min(3, "Mật khẩu phải có ít nhất 3 ký tự")
     .max(20, "Mật khẩu không được vượt quá 20 ký tự")
-    .required("Vui lòng nhập tên người dùng"),
-
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9!@#$%^&*()_+|~=\`{}[\]:";'<>?,./\\]).+$/,
+      "Mật khẩu phải chứa ít nhất một chữ hoa, một chữ thường và một số hoặc ký tự đặc biệt"
+    ),
 });
