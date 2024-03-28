8ede7f2ff21fa26558fce04532b3367a2cc0f14a
ToThienHung-NLU <20130274@st.hcmuaf.edu.vn>
2024-03-28 20:27:40 +0700

    update validation_2

--- src/schema/formSchema.js
+++ src/schema/formSchema.js
@@ -4,14 +4,14 @@ export const registerSchema = yup.object().shape({
   username: yup
     .string()
     .required("Vui lòng nhập tên người dùng")
-    .min(3, "Tên người dùng phải có ít nhất 3 ký tự")
-    .max(20, "Tên người dùng không được vượt quá 20 ký tự"),
+    .min(8, "Tên người dùng phải có ít nhất 8 ký tự")
+    .max(32, "Tên người dùng không được vượt quá 32 ký tự"),
 
   password: yup
     .string()
     .required("Vui lòng nhập mật khẩu")
-    .min(3, "Mật khẩu phải có ít nhất 3 ký tự")
-    .max(20, "Mật khẩu không được vượt quá 20 ký tự")
+    .min(8, "Mật khẩu phải có ít nhất 8 ký tự")
+    .max(32, "Mật khẩu không được vượt quá 32 ký tự")
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9!@#$%^&*()_+|~=\`{}[\]:";'<>?,./\\]).+$/,
       "Mật khẩu phải chứa ít nhất một chữ hoa, một chữ thường và một số hoặc ký tự đặc biệt"
